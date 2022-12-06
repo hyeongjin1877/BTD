fa51a2b4916693a186e8e6c7b5f8333eafe6feb5
Jeongwook Jang <oogicar45@gmail.com>
2022-12-07 00:13:58 +0900

    폴더 정

--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "myProject.h"
-
-int main(void)
-{
-    printf("Chief name is 김형진\n");
-    printMyName1();
-    printMyName2();
-}
--- myProject.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MYPROJECT_H_
-#define _MYPROJECT_H_
-
-
-int printMyName1(void);
-int printMyName2(void);
-
-#endif
