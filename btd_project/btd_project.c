b4f367a73859985e26a058e164aaf4816bf91c3b
Jeongwook Jang <oogicar45@gmail.com>
2022-12-11 02:32:03 +0900

    무궁화 꽃이 피었습니다 중간완

--- btd_project/btd_project.c
+++ btd_project/btd_project.c
@@ -11,12 +11,11 @@
 
 #include "button.h"
 #include "fnd.h"
-#include "textlcddrv.h"
 #include "colorled.h"
 #include "led.h"
+#include "textlcd.h"
 
-#define FND_DRIVER_NAME		"/dev/perifnd"
-#define LED_DRIVER_NAME "/dev/periled"
+
 #define MODE_STATIC_DIS		0
 static int msgID;
 BUTTON_MSG_T rcv;
@@ -27,7 +26,9 @@ static int result=0;
 static int success= 0;
 static int btn = 0;
 static int theend = 0;
-
+static int mugunghwa_end = 0;
+static int stage3_end = 0;
+static int step = 0;
 
 void *thread_object_0(){            // 스레드 사용해서 컬러 led 별개 동작
    pwmSetPercent(0, 0);
@@ -53,86 +54,87 @@ void *thread_object_0(){            // 스레드 사용해서 컬러 led 별개 
     sleep(5);
 }
 
-
 void *thread_object_2(){
-    while(1){
-		if(timer_end == 1){
-				break;
-		}
+    while(stage3_end != 1){
 		msgrcv(msgID, &rcv.keyInput, sizeof(rcv.keyInput), 0, 0);
 		if(rcv.keyInput == 3){
-			btn++;
-			if( btn == 100){
-				success = 1;
-				printf("Stage Clear !! \n\n");
-				break;
-			}
-			else if(btn %2 == 1){
-				if(timer_end == 1){
-					break;
-				}
-				printf("Key pressed for %d times\n", btn/2+1);
-			}
-		}
+            if(mugunghwa_end == 1){
+                lcdtextWrite("Stage 3", "FAILED");
+		        printf("Stage Failed !! \n");
+                system("sudo amixer sset 'Speaker' 40%");
+                system("sudo aplay ./gunsound.wav");
+                stage3_end = 1;
+                break;
+            }
+           else if(mugunghwa_end == 0){
+                 if(btn == 200){
+                    printf("Stage 1 Clear !! \n\n");
+                    lcdtextWrite("Stage 3", "CLEARED");
+                    stage3_end = 1;
+                    break;
+                }
+                btn++;
+                if(btn %2 == 1){
+                    printf("Key pressed for %d times\n", btn/2+1);
+                }
+            }
+        }
 	}
 }
 
-void *thread_object_1(){            // 스레드 사용해서 fnd 에 30초 타이머 출력
-   for(t=30; t>=0; t--){
-      mode=MODE_STATIC_DIS;
-      fndDisp(t,mode);
-      sleep (1);
-   }
-   	timer_end=1;
-    if(success == 0){
-		printf("Stage Failed !! \n");
-   }
-}
-
 int main(void){
-
+    btn = 0;
     pthread_t thread[2];
 	int msdID = msgget (MESSAGE_ID, IPC_CREAT|0666);
 
 	buttonInit();
     pwmLedInit();
     ledLibInit();
+    lcdtextInit();
 
 	printf("\nPRESS Search Button 50 times in 30 sec\n\n");
+    lcdtextWrite("GAME START", "");
 
-	pthread_create(&thread[0],NULL,thread_object_0,NULL);
-    pthread_create(&thread[1],NULL,thread_object_1,NULL);
+    while(1){
+        int returnValue = 0;
+        returnValue = msgrcv(msgID, &rcv, sizeof(rcv) - sizeof(long int), 0, IPC_NOWAIT);
+        if(returnValue == -1) break;
+    }
+	//pthread_create(&thread[0],NULL,thread_object_0,NULL);
+    //pthread_create(&thread[1],NULL,thread_object_1,NULL);
     pthread_create(&thread[2],NULL,thread_object_2,NULL);
 
     printf("\n\naudio start\n\n");
-    system("sudo amixer sset 'Speaker' 20%");
-    usleep(1850000);
-    system("sudo aplay ./bgm.wav"); 
-
-    /*
-    pid_t pid;
-
-    pid = fork();
-
-    if(pid == 0){
-        printf("\n\naudio start\n\n");
-        system("sudo amixer sset 'Speaker' 20%");
-        usleep(1850000);
-        system("sudo aplay ./bgm.wav"); 
+    
+    while(stage3_end != 1){
+        if(step == 3) {
+            if(btn < 200){
+                lcdtextWrite("Stage 3", "FAILED");
+		        printf("Stage Failed !! \n");
+                system("sudo amixer sset 'Speaker' 40%");
+                system("sudo aplay ./gunsound.wav");
+               stage3_end = 1;
+              break;
+            }
+        }
+        step ++;
+        mugunghwa_end = 0;
+        system("sudo amixer sset 'Speaker' 40%");
+        system("sudo aplay ./redlightgreenlight.wav");
+        mugunghwa_end = 1;
+        sleep(3);
     }
-    else if(pid > 0){
-        usleep(1200000);
-        
-        
-    }
-    */
+    //system("sudo amixer sset 'Speaker' 20%");
+    //usleep(1850000);
+    //system("sudo aplay ./bgm.wav"); 
 
-    pthread_join(thread[0], NULL);
-    pthread_join(thread[1], NULL);
+    //pthread_join(thread[0], NULL);
+    //pthread_join(thread[1], NULL);
     pthread_join(thread[2], NULL);
     pwmInactiveAll();
     buttonExit();
     ledLibExit();
-     
+    lcdtextExit();
+
     return 0;
 }
